09e44249c98220686a8c8626a56987dc63b4e7b4
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-11-03 20:58:48 -0800

    Completed 04_REMOVEFROMARRAY all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    //While going over each argument passed into the function,
+    //first argument is the array to be editted,
+    //arguments after will be values to be removed
+    //while going over each value in array, remove any value that matches the value to be removed
+    
+    for(let i = 1; i < args.length; i++){ //iterate through each argument after the first
+        let removeVal = args[i];
+        for(let j = args[0].length -1; j >= 0; j--){ //iterate through each value in array and remove matches
+            if(args[0][j] === removeVal){
+                args[0].splice(j,1);
+            }
+        }
+    }
+    return args[0];
 };
 
 // Do not edit below this line
