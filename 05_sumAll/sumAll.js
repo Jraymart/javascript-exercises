de719bf4d9af44f9197d1ed4ad8be84474be65b4
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-11-03 22:14:05 -0800

    Completed 05_SUMALL all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    //sum(n) = n(n+1)/2
+    //sum(start,end) = sum(end) - sum(start - 1)
+    if(Number.isInteger(start) && Number.isInteger(end) && start >= 0 && end >= 0){
+        if (start > end){
+            let temp = start;
+            start = end;
+            end = temp;
+        };
+        const sumEnd = (end *(end + 1))/2;
+        const sumStart = ((start -1) * start)/2;
+        return sumEnd - sumStart;
+    }
+    else {return "ERROR";}
 };
 
 // Do not edit below this line
