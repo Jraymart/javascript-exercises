07d30c75fff462282b98540b719ac5eabffdd0b0
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-11-04 17:11:45 -0800

    Completed 06_LEAPYEARS all tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //Leap Years are divisible by 4
+    //Years divisible by 100 are not leap years
+    //Years divisible by 400 are leap years
+    if (year % 4 === 0 && (!(year % 100 === 0) || year % 400 === 0)){
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
