021e979f1b0b1b4cb2eabf19544cf97e56c52d53
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-11-04 19:18:48 -0800

    Completed 09_PALINDROMES all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //convert passed string to all lowercase and remove any punctuation or spaces
+    //convert string to array reverse it then join again
+    //compare cleaned string with reverse string
+    let cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g,"");
+    let reversedStr = cleanedStr.split('').reverse().join('');
+
+    if (reversedStr === cleanedStr){
+        return true;
+    }
+    return false;
 
 };
 
