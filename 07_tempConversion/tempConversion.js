25ae38edbe313f50b25f1029fd1c4a4441f75ad5
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-11-04 17:23:00 -0800

    Completed 07_TEMPCONVERSION all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //celsius formula -> (n - 32) * (5/9) * C
+  return Math.round(((fahrenheit-32)*(5/9))*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  //fahrenheit formula -> (n * 9/5 + 32)* F
+  Math.round(((fahrenheit*(5/9))+32 )*10)/10;
 };
 
 // Do not edit below this line
