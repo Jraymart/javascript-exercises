72817d45daef54c42fb8846f010b36bef41dfd1c
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-11-09 19:44:08 -0800

    Completed 10_Fibonacci all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+    //fibonacci sequence is x(n) = x(n-1) + x(n-2)
+    if (term < 0){
+        return "OOPS";
+    }
+    const fib = [0,1];
+    for (let i = 2; i <= term; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[term];
 };
 
 // Do not edit below this line
