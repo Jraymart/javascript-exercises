9ac6d453892af292d5da75a6f6744cd6c11c330b
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-11-09 20:28:34 -0800

    Completed 12_FindTheOldest all tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+   return people.reduce((oldest, current) => {
+        const oldAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldAge < currentAge ? current : oldest;
+    });
 };
 
+const getAge = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
+
+
+  
 // Do not edit below this line
 module.exports = findTheOldest;
